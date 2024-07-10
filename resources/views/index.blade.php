4431d3a556fb1f3a3edb38ba9ac39d37aa9191c0
Deepu Gupta <82046692+deepug1@users.noreply.github.com>
2024-07-10 11:42:59 +0530

    push

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<html lang="en" class="hydrated">
+
+<head>
+    <title>Awesome Title</title>
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="root">
+        <main class="container">
+            <h1 class="title">Awesome Todos</h1>
+            <form class="form"><input type="text" placeholder="Enter a new todo..." class="form__input" required=""
+                    value=""><button class="form__button" type="submit">Create Todo</button></form>
+            <div class="todos">
+                <div class="todo">
+                    <p>dfdfbf</p>
+                    <div class="mutations">
+                        <button class="todo__status">☐</button>
+                        <button class="todo__delete">🗑️</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
